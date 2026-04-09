84fe6649d646e13fbe718c812afb7dbd8c12db98
Jaimealexis42 <alexisoficina42@gmail.com>
2026-04-09 09:36:09 -0230

    Add Tawk.to live chat

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,22 @@ export default function RootLayout({
       <head>
         <Script src="https://checkout.culqi.com/js/v4" strategy="beforeInteractive" />
       </head>
-      <body className="min-h-full flex flex-col bg-gray-100">{children}</body>
+      <body className="min-h-full flex flex-col bg-gray-100">
+        {children}
+        <Script id="tawkto" strategy="afterInteractive">
+          {`
+            var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+            (function(){
+              var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+              s1.async=true;
+              s1.src='https://embed.tawk.to/69d7917a03af4e1c38194370/1jlp10hp1';
+              s1.charset='UTF-8';
+              s1.setAttribute('crossorigin','*');
+              s0.parentNode.insertBefore(s1,s0);
+            })();
+          `}
+        </Script>
+      </body>
     </html>
   );
 }
